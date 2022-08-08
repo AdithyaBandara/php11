9da20ae15f39f89d56580cb0c9b725d18951fe2a
Adithya Bandara <104627196+AdithyaBandara@users.noreply.github.com>
2022-08-08 17:07:59 +0530

    2nd

--- signin.php
+++ signin.php
@@ -2,7 +2,7 @@
 $emi= $_POST('e','${email}');
 $pw =$_POST('p','${password}');
 
-if( $emi == "adithyabandara@gmail.com" & $pw = "123"){
+if( $emi == "adithyabandara@gmail.com" & $pw == "123"){
     echo ("Login Sucess");
 }else{
     echo("Invalid Email or Password");
